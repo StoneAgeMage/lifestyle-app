ffee66a03f6c89946c0300b1ec77ad811ed0512c
StoneAgeMage <captainkirkatron@gmail.com>
2026-06-29 20:11:53 -0700

    Fix iOS double-tap zoom: user-scalable=no viewport, touch-action:manipulation on buttons

--- service-worker.js
+++ service-worker.js
@@ -29,7 +29,7 @@
 
 // ---- Version ----
 // This is the ONLY string you need to change when deploying updates.
-const CACHE_NAME = 'rowing-v68';
+const CACHE_NAME = 'rowing-v69';
 
 // ---- Files to cache ----
 // Every file here is fetched and stored during the install step.
